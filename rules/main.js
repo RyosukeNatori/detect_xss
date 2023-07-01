c04ada037bbb3fe99b161aadeac93f1178918b25
Ryosuke Natori <nryosuke2001@gmail.com>
2023-07-02 04:28:27 +0900

    スコープオブジェクトのグローバルの変数宣言部を作った

--- rules/main.js
+++ rules/main.js
@@ -1,5 +1,4 @@
 import fs from 'fs';
-// import path from 'path';
 import engine from 'php-parser';
 
 const parser = new engine({
@@ -21,18 +20,26 @@ const parsedEval = parser.parseEval('echo "Hello World";');
 
 const parsedToken = parser.tokenGetAll(phpFile);
 
-const setParent = (node) => {
-  const shouldSetParents = ['body', 'left', 'right', 'what', 'offset', 'test'];
+const setParent = (ast) => {
+  const shouldSetParents = [
+    'body',
+    'left',
+    'right',
+    'what',
+    'offset',
+    'test',
+    'expression',
+  ];
 
   const shouldSetParentsFor = ['expressions', 'children', 'variables'];
 
-  const keys = Object.keys(node);
+  const keys = Object.keys(ast);
 
   keys
     .filter((key) => shouldSetParentsFor.includes(key))
     .forEach((key) => {
-      node[key].forEach((child) => {
-        child.parent = node;
+      ast[key].forEach((child) => {
+        child.parent = ast;
         setParent(child);
       });
     });
@@ -40,10 +47,41 @@ const setParent = (node) => {
   keys
     .filter((key) => shouldSetParents.includes(key))
     .forEach((key) => {
-      node[key].parent = node;
-      setParent(node[key]);
+      ast[key].parent = ast;
+      setParent(ast[key]);
     });
 };
 setParent(parsedCode);
 
-console.log(parsedCode.children);
+const buildScopeObject = (ast) => {
+  const templateScope = {
+    kind: '',
+    parent: null,
+    children: [],
+    variables: new Map(),
+    functions: [],
+  };
+
+  const scope = templateScope;
+  scope.kind = 'global';
+
+  for (const child of ast.children) {
+    switch (child.kind) {
+      case 'expressionstatement': {
+        if (scope.variables.has(child.expression.left.name)) {
+          scope.variables.get(child.expression.left.name).push({
+            ast: child.expression.left,
+            location: child.expression.left.loc,
+          });
+        } else {
+          scope.variables.set(child.expression.left.name, [
+            { ast: child.expression.left, location: child.expression.left.loc },
+          ]);
+        }
+      }
+    }
+  }
+  return scope;
+};
+const scope = buildScopeObject(parsedCode);
+// console.log();
