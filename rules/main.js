7c0a0c46e37e1279c47c1f9ede6cbf5149219fa8
RyosukeNatori <nryosuke2001@gmail.com>
2023-08-29 20:03:09 +0900

    1

--- rules/main.js
+++ rules/main.js
@@ -59,38 +59,63 @@ const buildScopeObject = ({ ast, target, parent }) => {
     childrenScopes: [],
     variables: new Map(),
     functions: [],
+    location: {
+      startLine: '',
+      endLine: '',
+    },
   };
   const templateFunction = {
     kind: 'function',
+    name: '',
+    childrenScopes: [],
+    variables: new Map(),
+    functions: [],
   };
-  console.log(ast);
 
   switch (ast.kind) {
     case 'program': {
       scope = templateScope;
+      scope.location.startLine = ast.loc.start.line;
+      scope.location.endLine = ast.loc.end.line;
 
       scope.kind = 'global';
       for (const child of ast.children) {
         switch (child.kind) {
           case 'expressionstatement': {
-            if (scope.variables.has(child.expression.left.name)) {
-              scope.variables.get(child.expression.left.name).push({
-                ast: child.expression.left,
-                location: child.expression.left.loc,
-              });
-            } else {
-              scope.variables.set(child.expression.left.name, [
-                {
-                  ast: child.expression.left,
-                  location: child.expression.left.loc,
-                },
-              ]);
+            switch (child.expression.kind) {
+              case 'assign': {
+                if (scope.variables.has(child.expression.left.name)) {
+                  scope.variables.get(child.expression.left.name).push({
+                    ast: child.expression.left,
+                    location: child.expression.left.loc,
+                  });
+                } else {
+                  scope.variables.set(child.expression.left.name, [
+                    {
+                      ast: child.expression.left,
+                      location: child.expression.left.loc,
+                    },
+                  ]);
+                }
+                break;
+              }
             }
             break;
           }
           case 'if': {
-            console.log(child);
-            buildScopeObject({ ast: child, target: scope.childrenScopes });
+            buildScopeObject({
+              ast: child,
+              target: scope.childrenScopes,
+              parent: scope,
+            });
+            break;
+          }
+          case 'function': {
+            buildScopeObject({
+              ast: child,
+              target: scope.functions,
+              parent: scope,
+            });
             break;
           }
         }
@@ -99,11 +124,89 @@ const buildScopeObject = ({ ast, target, parent }) => {
     }
     case 'if': {
       const ifScope = templateScope;
+      ifScope.kind = 'if';
+      ifScope.parent = parent;
+      for (const child of ast.body.children) {
+        switch (child.kind) {
+          case 'expressionstatement': {
+            // console.log(child);
+            switch (child.expression.kind) {
+              case 'assign': {
+                if (ifScope.variables.has(child.expression.left.name)) {
+                  ifScope.variables.get(child.expression.left.name).push({
+                    ast: child.expression.left,
+                    location: child.expression.left.loc,
+                  });
+                } else {
+                  ifScope.variables.set(child.expression.left.name, [
+                    {
+                      ast: child.expression.left,
+                      location: child.expression.left.loc,
+                    },
+                  ]);
+                }
+                break;
+              }
+            }
+            break;
+          }
+          case 'if': {
+            buildScopeObject({
+              ast: child,
+              target: ifScope.childrenScopes,
+              parent: ifScope,
+            });
+            break;
+          }
+        }
+      }
+      target.push(ifScope);
+      break;
+    }
+    case 'function': {
+      const functionScope = templateFunction;
+      functionScope.name = ast.name.name;
+      functionScope.parent = parent;
+      for (const child of ast.body.children) {
+        switch (child.kind) {
+          case 'expressionstatement': {
+            switch (child.expression.kind) {
+              case 'assign': {
+                if (functionScope.variables.has(child.expression.left.name)) {
+                  functionScope.variables.get(child.expression.left.name).push({
+                    ast: child.expression.left,
+                    location: child.expression.left.loc,
+                  });
+                } else {
+                  functionScope.variables.set(child.expression.left.name, [
+                    {
+                      ast: child.expression.left,
+                      location: child.expression.left.loc,
+                    },
+                  ]);
+                }
+                break;
+              }
+            }
+            break;
+          }
+          case 'if': {
+            buildScopeObject({
+              ast: child,
+              target: functionScope.childrenScopes,
+              parent: functionScope,
+            });
+            break;
+          }
+        }
+      }
+      target.push(functionScope);
+      break;
     }
   }
   return scope;
 };
 
-const hoge = buildScopeObject({ ast: parsedCode, target: '' });
+const hoge = buildScopeObject({ ast: parsedCode, target: '', parent: '' });
 
 console.log(hoge);
