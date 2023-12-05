27846c9a2f176c2cebc8f78f5f35dfad83f37c88
RyosukeNatori <nryosuke2001@gmail.com>
2023-12-05 16:43:59 +0900

    easyphp

--- sample/easy.php
+++ sample/easy.php
@@ -1,14 +1,8 @@
 <html>
-
-<body>
-  <?php
-  $text = `cat /tmp/tainted.txt`;
-  $echoString = $text;
-  if ($echoString) {
-    echo '<div>' . $echoString . '</div>';
-  }
-  print_r($text);
-  ?>
-</body>
-
+  <body>
+    <?php
+    $text = $_GET['UserData'];
+    echo '<div>' . $text . '</div>';
+    ?>
+  </body>
 </html>
