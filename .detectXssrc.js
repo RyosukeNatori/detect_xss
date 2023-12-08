03e71114a496ce9e6143ffaae957b652dba2eb4f
RyosukeNatori <nryosuke2001@gmail.com>
2023-12-08 12:03:57 +0900

    .detectXssrcの.git削除

--- .detectXssrc.js
+++
@@ -1,9 +0,0 @@
-const setConfig = () => {
-  return {
-    systemRootPath:
-      '/Users/ryosuke/project/php_and_html_parser/src/detectXss.js',
-    systemFunctionName: 'detectXss',
-  };
-};
-
-module.exports = setConfig;
